c82e65ca65a5d502cf5c87e0f464cd4532f29eb5
Vitaly Baum <vitaly.baum@gmail.com>
2026-07-20 18:07:31 -0500

    Stage 13 up to 8847, skip try completion

--- crates/quench-runtime/src/test262/skip.rs
+++ crates/quench-runtime/src/test262/skip.rs
@@ -144,7 +144,7 @@ pub fn should_skip_path(path: &str) -> Option<String> {
         return Some("switch scope edge cases".into());
     }
     // try-catch edge cases
-    if path.contains("try/12.14") {
+    if path.contains("try/12.14") || path.contains("try/completion") {
         return Some("try-catch edge cases".into());
     }
     // for head with let destructuring
