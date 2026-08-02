e0de387d0a1467f0f19fb7b249158e1a9c7ff014
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:41:59 -0500

    add filehandle file io methods

---
+++ tests/node-compat/stage-145/filehandle-fileio.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+(async () => {
+  const path = `/tmp/quench-node-stage-145-${process.pid}`;
+  const handle = await fs.promises.open(path, 'w+');
+  await handle.writeFile('hello');
+  if (await handle.readFile('utf8') !== 'hello') throw new Error('filehandle readFile mismatch');
+  await handle.close();
+})().then(() => undefined);
