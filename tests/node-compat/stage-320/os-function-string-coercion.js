c8f6c665a904c03b9d33e142c9b2c97eaac6acd9
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 15:13:17 -0500

    support os function string coercion

---
+++ tests/node-compat/stage-320/os-function-string-coercion.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const os = require("os");
+
+for (const name of [
+  "hostname",
+  "homedir",
+  "release",
+  "type",
+  "endianness",
+  "tmpdir",
+  "arch",
+  "platform",
+  "version",
+  "machine",
+]) {
+  assert.strictEqual(`${os[name]}`, os[name]());
+}
