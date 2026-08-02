453900795e51c62ba2852adcc7fbb2720401b09f
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:42:39 -0500

    add filehandle append method

---
+++ tests/node-compat/stage-146/filehandle-append.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+(async () => {
+  const path = `/tmp/quench-node-stage-146-${process.pid}`;
+  fs.writeFileSync(path, 'a');
+  const handle = await fs.promises.open(path, 'a');
+  await handle.appendFile('b');
+  await handle.close();
+  if (fs.readFileSync(path, 'utf8') !== 'ab') throw new Error('filehandle append mismatch');
+})().then(() => undefined);
