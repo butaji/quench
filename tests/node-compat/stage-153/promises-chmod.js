687c9ef5eb28e133a8fffb8d5bbaa10fcc67f8a8
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:48:04 -0500

    add promise chmod

---
+++ tests/node-compat/stage-153/promises-chmod.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+(async () => {
+  const path = `/tmp/quench-node-stage-153-${process.pid}`;
+  fs.writeFileSync(path, 'mode');
+  await fs.promises.chmod(path, 0o600);
+  if ((fs.statSync(path).mode & 0o777) !== 0o600) throw new Error('promise chmod mismatch');
+})().then(() => undefined);
