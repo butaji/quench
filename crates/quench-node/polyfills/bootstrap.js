8b381ec60128242e5bd488691179963e596db3a7
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:45:55 -0500

    add promise realpath

--- crates/quench-node/polyfills/bootstrap.js
+++ crates/quench-node/polyfills/bootstrap.js
@@ -732,6 +732,7 @@ globalThis.__nodeFs.promises = {
   opendir: (value, options) => Promise.resolve().then(() => globalThis.__nodeFs.opendirSync(value)),
   symlink: (target, link, type) => new Promise((resolve, reject) => globalThis.__nodeFs.symlink(target, link, type, (error) => error ? reject(error) : resolve())),
   readlink: (value, options) => new Promise((resolve, reject) => globalThis.__nodeFs.readlink(value, options, (error, result) => error ? reject(error) : resolve(result))),
+  realpath: (value, options) => Promise.resolve().then(() => globalThis.__nodeFs.realpathSync(value, options)),
   readv: (fd, buffers, position) => Promise.resolve().then(() => { const bytesRead = globalThis.__nodeFs.readvSync(fd, buffers, position); return { bytesRead, buffers }; }),
   writev: (fd, buffers, position) => Promise.resolve().then(() => { const bytesWritten = globalThis.__nodeFs.writevSync(fd, buffers, position); return { bytesWritten, buffers }; }),
   mkdir: (value) => Promise.resolve().then(() => globalThis.__nodeFs.mkdirSync(value)),
