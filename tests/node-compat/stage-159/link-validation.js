257d13b1ecd0326819d969f0a1c6d44d3adadbc7
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:52:22 -0500

    validate hard link paths

---
+++ tests/node-compat/stage-159/link-validation.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+for (const value of [false, 1, [], {}, null, undefined]) {
+  try { fs.linkSync(value, ''); throw new Error('accepted invalid source'); }
+  catch (error) { if (error.code !== 'ERR_INVALID_ARG_TYPE') throw error; }
+  try { fs.linkSync('', value); throw new Error('accepted invalid target'); }
+  catch (error) { if (error.code !== 'ERR_INVALID_ARG_TYPE') throw error; }
+}
