3482dafbeeba60a9cdb46e1801acfcb91d109a86
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 15:22:17 -0500

    tighten util format object regression

--- tests/node-compat/stage-328/util-format-object.js
+++ tests/node-compat/stage-328/util-format-object.js
@@ -1,4 +1,8 @@
 const util = require("util");
 const value = { foo: "bar", foobar: 1, func: function () {} };
 const output = util.format("%o", value);
-if (!output.includes("[Function: func]")) throw new Error(output);
+if (
+  output !==
+  "{\n  foo: 'bar',\n  foobar: 1,\n  func: [Function: func] {\n    [length]: 0,\n    [name]: 'func',\n    [prototype]: { [constructor]: [Circular *1] }\n  }\n}"
+)
+  throw new Error(output);
