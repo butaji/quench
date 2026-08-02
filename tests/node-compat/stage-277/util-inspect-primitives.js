cdba16dd605319ed10641329c852a38e5429b9c1
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 14:31:11 -0500

    add util inspect primitive formatting

---
+++ tests/node-compat/stage-277/util-inspect-primitives.js
@@ -0,0 +1,7 @@
+const { inspect } = require("util");
+
+if (inspect("hello") !== "'hello'") throw new Error("string inspect failed");
+if (inspect(function named() {}) !== "[Function: named]")
+  throw new Error("named function inspect failed");
+if (inspect(() => {}) !== "[Function (anonymous)]")
+  throw new Error("anonymous function inspect failed");
