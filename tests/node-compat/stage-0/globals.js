d7b87dc6314440e43edd6ffca633796520a6d9e0
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 09:32:54 -0500

    add staged process executable identity

--- tests/node-compat/stage-0/globals.js
+++ tests/node-compat/stage-0/globals.js
@@ -1,4 +1,4 @@
 if (global !== globalThis) throw new Error('global alias');
-if (process.argv[0] !== 'quench-node') throw new Error('process.argv');
+if (process.argv[0] !== process.execPath) throw new Error('process.argv');
 if (typeof process.nextTick !== 'function') throw new Error('process.nextTick');
 if (typeof Buffer !== 'function') throw new Error('Buffer');
