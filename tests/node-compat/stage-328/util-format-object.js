fb74c14de944bdf39e3eed96b942368ca3710f3c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 15:23:09 -0500

    format nested util objects and arrays

--- tests/node-compat/stage-328/util-format-object.js
+++ tests/node-compat/stage-328/util-format-object.js
@@ -3,6 +3,6 @@ const value = { foo: "bar", foobar: 1, func: function () {} };
 const output = util.format("%o", value);
 if (
   output !==
-  "{\n  foo: 'bar',\n  foobar: 1,\n  func: [Function: func] {\n    [length]: 0,\n    [name]: 'func',\n    [prototype]: { [constructor]: [Circular *1] }\n  }\n}"
+  "{\n  foo: 'bar',\n  foobar: 1,\n  func: <ref *1> [Function: func] {\n    [length]: 0,\n    [name]: 'func',\n    [prototype]: { [constructor]: [Circular *1] }\n  }\n}"
 )
   throw new Error(output);
