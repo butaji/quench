40057acffa0360b222cba86cebe934a98211fd82
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:01:05 -0500

    add staged link callback compatibility

---
+++ tests/node-compat/stage-107/link-callbacks.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const common = require('../common');
+
+const target = '/tmp/quench-node-stage-107-target';
+const link = '/tmp/quench-node-stage-107-link';
+fs.writeFileSync(target, 'link');
+fs.symlink(target, link, common.mustSucceed(() => {
+  fs.readlink(link, common.mustSucceed((value) => {
+    if (value !== target) throw new Error('readlink mismatch');
+  }));
+}));
