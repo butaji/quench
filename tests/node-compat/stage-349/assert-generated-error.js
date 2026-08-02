51fc41e1b11de93e73c3069e6275ce9964e0c499
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 15:34:15 -0500

    add generated assertion error metadata

---
+++ tests/node-compat/stage-349/assert-generated-error.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+const result = assert.throws(
+  () =>
+    assert.throws(() => {
+      throw new TypeError({});
+    }, assert.AssertionError),
+  assert.AssertionError,
+);
+if (result.code !== "ERR_ASSERTION" || result.generatedMessage !== true)
+  throw new Error("missing assertion metadata");
