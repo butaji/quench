a429e3fba13c4a58c5dae66126261ca01471912e
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 14:58:16 -0500

    validate posix path arguments

---
+++ tests/node-compat/stage-299/path-posix-validation.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const path = require("path").posix;
+
+for (const value of [true, 7, null, {}, undefined, [], NaN]) {
+  assert.throws(() => path.join(value), { code: "ERR_INVALID_ARG_TYPE" });
+  assert.throws(() => path.normalize(value), { code: "ERR_INVALID_ARG_TYPE" });
+  assert.throws(() => path.isAbsolute(value), { code: "ERR_INVALID_ARG_TYPE" });
+  assert.throws(() => path.basename(value), { code: "ERR_INVALID_ARG_TYPE" });
+}
