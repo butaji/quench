f9f015e747685d2533a520385b91b4550db97b3c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:45:55 -0500

    add promise realpath

---
+++ tests/node-compat/stage-150/realpath-promise.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+(async () => {
+  const path = `/tmp/quench-node-stage-150-${process.pid}`;
+  fs.writeFileSync(path, 'x');
+  if (await fs.promises.realpath(path) !== fs.realpathSync(path)) throw new Error('promise realpath mismatch');
+  const result = await fs.promises.realpath(path, { encoding: 'buffer' });
+  if (!Buffer.isBuffer(result) || result.toString() !== fs.realpathSync(path)) throw new Error('promise realpath buffer mismatch');
+})().then(() => undefined);
