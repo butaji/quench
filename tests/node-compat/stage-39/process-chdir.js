4aa3b83cd23f5990f00331961ab3cabfe7c3c2dc
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 09:19:16 -0500

    add staged process chdir support

---
+++ tests/node-compat/stage-39/process-chdir.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const fs = require('fs');
+const folder = fs.mkdtempSync('/tmp/quench-node-');
+const before = process.cwd();
+process.chdir(folder);
+assert.strictEqual(process.cwd(), fs.realpathSync(folder));
+process.chdir(before);
+fs.rmdirSync(folder);
