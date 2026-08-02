2d9c1da5c08ae34f332a779cc8077c270dea373d
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 17:20:18 -0500

    fix realpath capability gate

--- tests/node-compat/stage-85/realpath.js
+++ tests/node-compat/stage-85/realpath.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const common = require('../common');
 
-if (common.canCreateSymLink()) throw new Error('unexpected symlink capability');
+if (!common.canCreateSymLink()) process.exit(0);
 const path = fs.realpathSync('.');
 if (!path.startsWith('/')) throw new Error('realpath must be absolute');
 fs.realpath('.', common.mustSucceed((value) => {
