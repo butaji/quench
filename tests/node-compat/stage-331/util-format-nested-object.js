710f791d4c23fda961bc76e69abb918dd553a579
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 15:23:53 -0500

    cover nested util format objects

---
+++ tests/node-compat/stage-331/util-format-nested-object.js
@@ -0,0 +1,8 @@
+const util = require("util");
+const value = { foo: "bar", foobar: { foo: "bar", func: function func() {} } };
+const output = util.format("%o", value);
+if (
+  !output.includes("foobar: {") ||
+  !output.includes("<ref *1> [Function: func]")
+)
+  throw new Error(output);
