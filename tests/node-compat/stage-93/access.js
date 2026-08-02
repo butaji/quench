4f2f82f85434328b0324bfe6fd50988061403ee3
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 10:28:52 -0500

    add staged filesystem access compatibility

---
+++ tests/node-compat/stage-93/access.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const common = require('../common');
+
+const path = '/tmp/quench-node-stage-93';
+fs.writeFileSync(path, 'access');
+fs.accessSync(path, fs.constants.R_OK);
+fs.access(path, fs.constants.R_OK, common.mustSucceed(() => {}));
+fs.promises.access(path).then(common.mustCall());
