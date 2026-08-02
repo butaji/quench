fb5ad9273023f4e712da5c4746031007f0e44a33
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:54:50 -0500

    enable symlink compatibility tests

---
+++ tests/node-compat/stage-163/symlink-type.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+try { fs.symlinkSync('', '', 'invalid'); throw new Error('accepted invalid symlink type'); }
+catch (error) { if (error.code !== 'ERR_INVALID_ARG_VALUE') throw error; }
