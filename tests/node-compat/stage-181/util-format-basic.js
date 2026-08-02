efc829590f5b2926c20865016ed4fd5c7500913e
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 12:21:23 -0500

    cover util format symbol coercion

--- tests/node-compat/stage-181/util-format-basic.js
+++ tests/node-compat/stage-181/util-format-basic.js
@@ -4,4 +4,8 @@ if (util.format([0]) !== '[ 0 ]') throw new Error('array format mismatch');
 if (util.format({ foo: 42 }) !== '{ foo: 42 }') throw new Error('object format mismatch');
 if (util.format('%i %f', '42.5', '1.5') !== '42 1.5') throw new Error('numeric format mismatch');
 if (util.format('%d', 42.0) !== '42') throw new Error(`d format mismatch: ${util.format('%d', 42.0)}`);
+const symbol = Symbol('foo');
+if (util.format(symbol) !== 'Symbol(foo)') throw new Error('symbol format mismatch');
+if (util.format('%s', symbol) !== 'Symbol(foo)') throw new Error('symbol string mismatch');
+if (util.format('%j', symbol) !== 'undefined') throw new Error('symbol json mismatch');
 if (util.format('foo', 'bar', 'baz') !== 'foo bar baz') throw new Error('extra argument format mismatch');
