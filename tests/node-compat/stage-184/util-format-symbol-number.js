5e963afcf557173c8c6f7fd91d760bf5c1d29629
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 12:26:19 -0500

    handle symbols in util numeric formats

---
+++ tests/node-compat/stage-184/util-format-symbol-number.js
@@ -0,0 +1,5 @@
+const util = require('util');
+
+if (util.format('%d', Symbol()) !== 'NaN') throw new Error('symbol decimal mismatch');
+if (util.format('%i', Symbol()) !== 'NaN') throw new Error('symbol integer mismatch');
+if (util.format('%f', Symbol()) !== 'NaN') throw new Error('symbol float mismatch');
