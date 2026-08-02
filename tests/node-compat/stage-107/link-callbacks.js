158a429f87ef0b1c8e016c23337878fc092f6229
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 17:21:58 -0500

    make symlink callback stage repeatable

--- tests/node-compat/stage-107/link-callbacks.js
+++ tests/node-compat/stage-107/link-callbacks.js
@@ -3,6 +3,7 @@ const common = require('../common');
 
 const target = '/tmp/quench-node-stage-107-target';
 const link = '/tmp/quench-node-stage-107-link';
+try { fs.unlinkSync(link); } catch (_) {}
 fs.writeFileSync(target, 'link');
 fs.symlink(target, link, common.mustSucceed(() => {
   fs.readlink(link, common.mustSucceed((value) => {
