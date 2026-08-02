b599cbf6b724c78ee6a59c18c2e9422347edb99a
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:27:28 -0500

    add common array buffer view helper

---
+++ tests/node-compat/stage-128/common-views.js
@@ -0,0 +1,4 @@
+const common = require('../common');
+const buffer = Buffer.from('abc');
+const views = common.getArrayBufferViews(buffer);
+if (views.length < 3 || views.some((view) => view.byteLength !== 3)) throw new Error('common views mismatch');
