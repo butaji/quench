3d0ba9ef7037c04a143d18ba43fe293526c11a7b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:39:54 -0500

    add filehandle truncate

---
+++ tests/node-compat/stage-142/filehandle-truncate.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+(async () => {
+  const path = `/tmp/quench-node-stage-142-${process.pid}`;
+  fs.writeFileSync(path, 'abcdef');
+  const handle = await fs.promises.open(path, 'r+');
+  await handle.truncate(2);
+  await handle.close();
+  if (fs.readFileSync(path, 'utf8') !== 'ab') throw new Error('filehandle truncate mismatch');
+})().then(() => undefined);
