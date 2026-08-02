ce0aa10355010c6ae6d30901bc86345564a6b391
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 11:48:31 -0500

    add promise file mutations

---
+++ tests/node-compat/stage-154/promises-mutations.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+(async () => {
+  const root = `/tmp/quench-node-stage-154-${process.pid}`;
+  const source = `${root}-source`;
+  const copy = `${root}-copy`;
+  const renamed = `${root}-renamed`;
+  fs.writeFileSync(source, 'copy');
+  await fs.promises.copyFile(source, copy);
+  await fs.promises.rename(copy, renamed);
+  if (fs.readFileSync(renamed, 'utf8') !== 'copy') throw new Error('promise mutation mismatch');
+  await fs.promises.unlink(renamed);
+})().then(() => undefined);
