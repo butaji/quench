eea183cb3cc35950b95f8291ccc3871ad7a2bcb7
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-03 00:05:18 -0500

    preserve cumulative cluster setup

--- crates/quench-node/src/main.rs
+++ crates/quench-node/src/main.rs
@@ -76,6 +76,7 @@ const BOOTSTRAP_PARTS: &[&str] = &[
     include_str!("../polyfills/bootstrap-parts/part-59.js"),
     include_str!("../polyfills/bootstrap-parts/part-60.js"),
     include_str!("../polyfills/bootstrap-parts/part-61.js"),
+    include_str!("../polyfills/bootstrap-parts/part-62.js"),
 ];
 static MKDTEMP_SEQUENCE: AtomicUsize = AtomicUsize::new(0);
 
