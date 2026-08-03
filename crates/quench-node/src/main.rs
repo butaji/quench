f8e443ef3269969ecc32e7e77e0ce1f6316913a2
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-03 01:21:48 -0500

    add fork child export stage

--- crates/quench-node/src/main.rs
+++ crates/quench-node/src/main.rs
@@ -93,6 +93,7 @@ const BOOTSTRAP_PARTS: &[&str] = &[
     include_str!("../polyfills/bootstrap-parts/part-76.js"),
     include_str!("../polyfills/bootstrap-parts/part-77.js"),
     include_str!("../polyfills/bootstrap-parts/part-78.js"),
+    include_str!("../polyfills/bootstrap-parts/part-79.js"),
 ];
 static MKDTEMP_SEQUENCE: AtomicUsize = AtomicUsize::new(0);
 
