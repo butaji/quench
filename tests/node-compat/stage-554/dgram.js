4e5e98e6163b7417dabcd3ee9041f88d80a10663
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 23:25:07 -0500

    add dgram compatibility surface stage

---
+++ tests/node-compat/stage-554/dgram.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const assert = require("assert");
+const dgram = require("dgram");
+
+const socket = dgram.createSocket("udp4");
+assert.strictEqual(socket.type, "udp4");
+socket.bind(0, "127.0.0.1");
+assert.strictEqual(socket.address().family, "IPv4");
+socket.send(Buffer.from("packet"), 9999, "127.0.0.1", (error) =>
+  assert.ifError(error)
+);
+socket.close();
+assert.strictEqual(typeof socket.unref, "function");
+
+console.log("dgram passed");
