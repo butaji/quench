93846c4ec2c8399851bcd57dfd50156792936a7b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 22:59:17 -0500

    add module compatibility surface stage

--- crates/quench-node/src/main.rs
+++ crates/quench-node/src/main.rs
@@ -41,6 +41,7 @@ const BOOTSTRAP_PARTS: &[&str] = &[
     include_str!("../polyfills/bootstrap-parts/part-24.js"),
     include_str!("../polyfills/bootstrap-parts/part-25.js"),
     include_str!("../polyfills/bootstrap-parts/part-26.js"),
+    include_str!("../polyfills/bootstrap-parts/part-27.js"),
 ];
 static MKDTEMP_SEQUENCE: AtomicUsize = AtomicUsize::new(0);
 
