f6abf3f74e495c856cf00177ebb068f40b9c91c9
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 23:28:10 -0500

    add test reporters unsupported boundary

--- crates/quench-node/src/main.rs
+++ crates/quench-node/src/main.rs
@@ -69,6 +69,7 @@ const BOOTSTRAP_PARTS: &[&str] = &[
     include_str!("../polyfills/bootstrap-parts/part-52.js"),
     include_str!("../polyfills/bootstrap-parts/part-53.js"),
     include_str!("../polyfills/bootstrap-parts/part-54.js"),
+    include_str!("../polyfills/bootstrap-parts/part-55.js"),
 ];
 static MKDTEMP_SEQUENCE: AtomicUsize = AtomicUsize::new(0);
 
