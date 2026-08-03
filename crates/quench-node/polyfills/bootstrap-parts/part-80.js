255400f00dbb8ead9c0afb0498f87a0fa996ee2a
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-03 01:27:13 -0500

    add child process stream methods

---
+++ crates/quench-node/polyfills/bootstrap-parts/part-80.js
@@ -0,0 +1,3 @@
+globalThis.__quench_bootstrap_fragments.push(
+  'const __quenchChildStreamRequire = globalThis.require;\nconst __quenchChildStreamModule = __quenchChildStreamRequire("child_process");\nconst __quenchOriginalChildStreamSpawn = __quenchChildStreamModule.spawn;\nconst __quenchEnsureStream = (stream) => { if (typeof stream.on !== "function") stream.on = () => stream; if (typeof stream.once !== "function") stream.once = stream.on; if (typeof stream.setEncoding !== "function") stream.setEncoding = () => stream; return stream; };\n__quenchChildStreamModule.spawn = (...args) => { const child = __quenchOriginalChildStreamSpawn(...args); __quenchEnsureStream(child.stdin); __quenchEnsureStream(child.stdout); __quenchEnsureStream(child.stderr); return child; };\n'
+);
--- crates/quench-node/src/main.rs
+++ crates/quench-node/src/main.rs
@@ -94,6 +94,7 @@ const BOOTSTRAP_PARTS: &[&str] = &[
     include_str!("../polyfills/bootstrap-parts/part-77.js"),
     include_str!("../polyfills/bootstrap-parts/part-78.js"),
     include_str!("../polyfills/bootstrap-parts/part-79.js"),
+    include_str!("../polyfills/bootstrap-parts/part-80.js"),
 ];
 static MKDTEMP_SEQUENCE: AtomicUsize = AtomicUsize::new(0);
 
