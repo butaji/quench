9b1622f5e73f84d17b608723fbaf02a87fb66e0b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-02 23:48:21 -0500

    add cluster scheduling constants stage

---
+++ crates/quench-node/polyfills/bootstrap-parts/part-58.js
@@ -0,0 +1,3 @@
+globalThis.__quench_bootstrap_fragments.push(
+  'const __quenchClusterRequire = globalThis.require;\nconst __quenchCluster = __quenchClusterRequire("cluster");\n__quenchCluster.SCHED_NONE = 1;\n__quenchCluster.SCHED_RR = 2;\n'
+);
