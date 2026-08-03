89cbfbd8f0964ffef89d7a292f762fe66bd5782c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-03 00:47:31 -0500

    add child process lifecycle events

---
+++ crates/quench-node/polyfills/bootstrap-parts/part-72.js
@@ -0,0 +1,3 @@
+globalThis.__quench_bootstrap_fragments.push(
+  'const __quenchChildEventOrderRequire = globalThis.require;\nconst __quenchChildEventOrder = __quenchChildEventOrderRequire("child_process");\nconst __quenchEventOrderSpawn = __quenchChildEventOrder.spawn;\n__quenchChildEventOrder.spawn = (...args) => { const child = __quenchEventOrderSpawn(...args); child.on = globalThis.__nodeEventEmitter.prototype.on; child.emit = ((emit) => (event, ...values) => { if (event === "exit" && !child.__spawnEmitted) { child.__spawnEmitted = true; emit.call(child, "spawn"); } return emit.call(child, event, ...values); })(child.emit); return child; };\n'
+);
