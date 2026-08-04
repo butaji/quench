4cb2c487444aa9966a261dae9746cbd98900a533
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 18:03:19 -0500

    Implement vm Script evaluation

---
+++ tests/node-compat/stage-1122/vm-script-evaluation.js
@@ -0,0 +1,5 @@
+const vm = require("vm");
+const context = vm.createContext();
+const script = new vm.Script('"passed";');
+if (script.runInContext(context) !== "passed")
+  throw new Error("Script evaluation failed");
