14a101fcf0edb5032d3b5cc8de248cfcd92a3057
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 03:30:26 -0500

    feat: add process stdio async disposal stage 780

---
+++ tests/node-compat/stage-780/process-stdio-async-dispose.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const assert = require("assert");
+const processApi = require("process");
+
+for (const stream of [processApi.stdout, processApi.stderr]) {
+  assert.strictEqual(typeof stream[Symbol.asyncDispose], "function");
+  assert.strictEqual(
+    stream[Symbol.asyncDispose].constructor.name,
+    "AsyncFunction"
+  );
+}
+
+console.log("process stdio async dispose passed");
