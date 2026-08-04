038a3cf30bc3c4681da7d59b1a6f9ae776940adf
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 00:54:53 -0500

    feat: cover process next tick stage 667

---
+++ tests/node-compat/stage-667/process-next-tick.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const assert = require("assert");
+const processApi = require("process");
+
+let received;
+processApi.nextTick(
+  (first, second) => {
+    received = [first, second];
+  },
+  "first",
+  2
+);
+queueMicrotask(() => assert.deepStrictEqual(received, ["first", 2]));
+
+console.log("process nextTick passed");
