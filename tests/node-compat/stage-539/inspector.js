cbf2ee46dee8715f84c821df27670397b16aaad9
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 05:15:08 -0500

    feat: add inspector api stage 855

--- tests/node-compat/stage-539/inspector.js
+++ tests/node-compat/stage-539/inspector.js
@@ -2,13 +2,13 @@
 
 const assert = require("assert");
 
-for (const name of [
-  "inspector",
-  "node:inspector",
-  "inspector/promises",
-  "node:inspector/promises"
-]) {
+for (const name of ["inspector", "node:inspector"]) {
+  const inspector = require(name);
+  assert.strictEqual(typeof inspector.open, "function");
+  assert.strictEqual(typeof inspector.Session, "function");
+}
+for (const name of ["inspector/promises", "node:inspector/promises"]) {
   assert.throws(() => require(name), { code: "ERR_UNKNOWN_BUILTIN_MODULE" });
 }
 
-console.log("inspector error passed");
+console.log("inspector api passed");
