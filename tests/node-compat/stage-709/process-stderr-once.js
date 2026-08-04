a4fd4a22bc72225f397766a650bc34fc29df661d
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 01:56:57 -0500

    feat: cover process stderr once stage 709

---
+++ tests/node-compat/stage-709/process-stderr-once.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const assert = require("assert");
+const processApi = require("process");
+
+const listener = () => {};
+assert.strictEqual(
+  processApi.stderr.once("drain", listener),
+  processApi.stderr
+);
+processApi.stderr.removeListener("drain", listener);
+
+console.log("process stderr once passed");
