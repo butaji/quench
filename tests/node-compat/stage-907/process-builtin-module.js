420a91033251b2eac635fb18e9fa2b2fff4c8bd0
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 06:44:37 -0500

    test: add process builtin module stage 907

---
+++ tests/node-compat/stage-907/process-builtin-module.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const assert = require("assert");
+
+assert.strictEqual(typeof process.getBuiltinModule, "function");
+const crypto = process.getBuiltinModule("crypto");
+assert.strictEqual(typeof crypto.createHash, "function");
+assert.strictEqual(
+  process.getBuiltinModule("definitely-not-a-module"),
+  undefined
+);
+
+console.log("process builtin module passed");
