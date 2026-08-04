3ce9cad2b5744a41a1ad0e52948c92ca5a5f666c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-03 19:42:20 -0500

    Fix Array.group length access ordering

--- builtins/Array.js
+++ builtins/Array.js
@@ -738,9 +738,9 @@ Array.prototype.findLastIndex = function ArrayFindLastIndex(callbackfn /*, thisA
 // Array.prototype.group (ES2025 §23.1.3.18)
 Array.prototype.group = function ArrayGroup(callbackfn /*, thisArg */) {
   if (this === null || this === undefined) throw ThrowTypeError("Array.prototype.group called on null or undefined");
-  if (!IsCallable(callbackfn)) throw ThrowTypeError("callbackfn is not a function");
   var O = ToObject(this);
   var len = ToLength(O.length);
+  if (!IsCallable(callbackfn)) throw ThrowTypeError("callbackfn is not a function");
   var thisArg = arguments.length > 1 ? arguments[1] : undefined;
   var groups = {};
   for (var k = 0; k < len; k++) {
