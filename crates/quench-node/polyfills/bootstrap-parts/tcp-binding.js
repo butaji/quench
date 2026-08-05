d45cc67f7a39c5302711189bb4bfb59e8a744897
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 23:58:18 -0500

    Expand TCP binding compatibility

--- crates/quench-node/polyfills/bootstrap-parts/tcp-binding.js
+++ crates/quench-node/polyfills/bootstrap-parts/tcp-binding.js
@@ -1,6 +1,11 @@
 globalThis.__quenchTcpBinding = () => {
   const TCP = class TCP {
+    constructor() {
+      this.fd = 0;
+    }
     setNoDelay() {}
+    listen() {}
+    close() {}
   };
-  return { TCP, TCPWrap: TCP };
+  return { TCP, TCPWrap: TCP, constants: { SOCKET: 1 } };
 };
