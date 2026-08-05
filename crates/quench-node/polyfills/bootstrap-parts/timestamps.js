09e5e0a1c3bbbbdb77578a8ca38b0e2def661285
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 00:48:56 -0500

    Complete utimes validation compatibility

--- crates/quench-node/polyfills/bootstrap-parts/timestamps.js
+++ crates/quench-node/polyfills/bootstrap-parts/timestamps.js
@@ -1,26 +1,48 @@
 globalThis.__nodeTimes ||= Object.create(null);
 const __nodeFsTimes = (value) => {
-  const path =
-    typeof value === "number"
-      ? globalThis.__nodeFdPaths[value]
-      : nodeFsPath(value);
-  if (!path) {
+  return __nodeFsPathOnly(value);
+};
+const __nodeFsSetTimes = (value, atime, mtime) => {
+  const path = __nodeFsTimes(value);
+  try {
+    globalThis.__quench_fs_kind(path);
+  } catch (_) {
     const error = new Error("ENOENT: no such file or directory");
     error.code = "ENOENT";
     throw error;
   }
-  return path;
-};
-const __nodeFsSetTimes = (value, atime, mtime) => {
-  const path = __nodeFsTimes(value);
   globalThis.__nodeTimes[path] = { atime: Number(atime), mtime: Number(mtime) };
 };
+const __nodeFsSetLinkTimes = (value, atime, mtime) => {
+  const path = __nodeFsPathOnly(value);
+  globalThis.__nodeTimes[path] = { atime: Number(atime), mtime: Number(mtime) };
+};
+const __nodeFsSetFdTimes = (fd, atime, mtime) => {
+  if (typeof fd !== "number") {
+    const error = new TypeError('The "fd" argument must be of type number');
+    error.code = "ERR_INVALID_ARG_TYPE";
+    throw error;
+  }
+  if (!Number.isInteger(fd) || fd < 0) {
+    const error = new RangeError(
+      'The value of "fd" is out of range. It must be >= 0 && <= 2147483647. Received -1'
+    );
+    error.code = "ERR_OUT_OF_RANGE";
+    throw error;
+  }
+  __nodeFsSetTimes(globalThis.__nodeFdPaths[fd], atime, mtime);
+};
 globalThis.__nodeFs.utimesSync = __nodeFsSetTimes;
-globalThis.__nodeFs.lutimesSync = __nodeFsSetTimes;
-globalThis.__nodeFs.futimesSync = __nodeFsSetTimes;
+globalThis.__nodeFs.lutimesSync = __nodeFsSetLinkTimes;
+globalThis.__nodeFs.futimesSync = __nodeFsSetFdTimes;
 const __nodeFsAsyncTimes = (method, value, atime, mtime, callback) => {
   if (typeof callback !== "function")
     throw new TypeError('The "callback" argument must be of type function');
+  try {
+    globalThis.__nodeFs[method](value, atime, mtime);
+  } catch (error) {
+    if (error.code !== "ENOENT") throw error;
+  }
   queueMicrotask(() => {
     try {
       globalThis.__nodeFs[method](value, atime, mtime);
