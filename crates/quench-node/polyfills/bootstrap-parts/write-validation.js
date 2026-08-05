62d7b3ea01458958e99ac7bab364d41fe0dcff9e
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 02:11:35 -0500

    Align writev callback overload

--- crates/quench-node/polyfills/bootstrap-parts/write-validation.js
+++ crates/quench-node/polyfills/bootstrap-parts/write-validation.js
@@ -19,3 +19,27 @@ const __nodeFsValidateWrite = (fd, buffer, callback) => {
     throw error;
   }
 };
+const __nodeFsValidateWritev = (fd, buffers, callback) => {
+  if (typeof callback !== "function") {
+    const error = new TypeError(
+      'The "callback" argument must be of type function'
+    );
+    error.code = "ERR_INVALID_ARG_TYPE";
+    throw error;
+  }
+  if (typeof fd !== "number") {
+    const error = new TypeError('The "fd" argument must be of type number');
+    error.code = "ERR_INVALID_ARG_TYPE";
+    throw error;
+  }
+  if (
+    !Array.isArray(buffers) ||
+    buffers.some((buffer) => !(buffer instanceof Uint8Array))
+  ) {
+    const error = new TypeError(
+      'The "buffers" argument must be an array of Buffer or Uint8Array'
+    );
+    error.code = "ERR_INVALID_ARG_TYPE";
+    throw error;
+  }
+};
