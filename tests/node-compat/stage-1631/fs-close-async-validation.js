03fa623cce799424a143d87e7b6a3574d29815f0
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 13:37:14 -0500

    Validate async filesystem close descriptors

---
+++ tests/node-compat/stage-1631/fs-close-async-validation.js
@@ -0,0 +1,8 @@
+const assert = require("node:assert");
+const fs = require("node:fs");
+
+assert.throws(() => fs.close("fd"), {
+  code: "ERR_INVALID_ARG_TYPE",
+  message: /Received type string \('fd'\)/
+});
+console.log("Filesystem async close validation passed");
