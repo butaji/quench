f8272d4bb654c7c6af7b1d989cade86dc2550967
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 04:18:32 -0500

    Validate Diffie-Hellman numbers

---
+++ tests/node-compat/stage-1390/crypto-dh-number-validation.js
@@ -0,0 +1,12 @@
+const assert = require("node:assert");
+const crypto = require("node:crypto");
+
+assert.throws(() => crypto.createDiffieHellman(13.37), {
+  code: "ERR_OUT_OF_RANGE",
+  name: "RangeError"
+});
+assert.throws(() => crypto.createDiffieHellman("abcdef", 13.37), {
+  code: "ERR_OUT_OF_RANGE",
+  name: "RangeError"
+});
+console.log("crypto DH number validation passed");
