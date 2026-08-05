ec19ef5eee5e8f8f121f45649b381406d51513d9
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 17:07:03 -0500

    Parse bracketed IPv6 authorities

---
+++ tests/node-compat/stage-1692/url-ipv6-authority.js
@@ -0,0 +1,4 @@
+const url = new URL("http://[2001::1]");
+if (url.href !== "http://[2001::1]/")
+  throw new Error(`Unexpected URL: ${url.href}`);
+console.log("URL IPv6 authorities do not create phantom ports");
