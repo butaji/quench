6ff179abe81173402a99dd6f86b4bd71ca4a12b1
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 19:12:14 -0500

    Strengthen VM realm property stage

--- tests/node-compat/stage-1150/vm-object-has-own.js
+++ tests/node-compat/stage-1150/vm-object-has-own.js
@@ -4,8 +4,14 @@ const outer = { inherited: true };
 const sandbox = Object.create(Object.create(outer));
 const context = vm.createContext(sandbox);
 const result = vm.runInContext(
-  "[typeof Object.hasOwn, Object.hasOwn(this, 'inherited')]",
+  "Object.defineProperty(Object.prototype, 'inner', {value: true, configurable: true}); [typeof Object.hasOwn, Object.hasOwn(this, 'inherited'), Object.hasOwn(this, 'inner'), 'inner' in this, Object.getOwnPropertyDescriptor(this, 'inner')]",
   context
 );
-if (result[0] !== "function" || result[1] !== false)
+if (
+  result[0] !== "function" ||
+  result[1] !== false ||
+  result[2] !== false ||
+  result[3] !== true ||
+  result[4] !== undefined
+)
   throw new Error(`unexpected Object.hasOwn result: ${result}`);
