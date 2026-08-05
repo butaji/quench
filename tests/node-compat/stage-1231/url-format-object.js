c34879122c28d09c29d09afba94e0701daccca0b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-04 23:22:40 -0500

    Improve legacy URL formatting compatibility

---
+++ tests/node-compat/stage-1231/url-format-object.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const { format } = require("url");
+
+assert.strictEqual(
+  format({
+    protocol: "http",
+    host: "a.com",
+    pathname: "a/b/c",
+    hash: "h",
+    search: "s"
+  }),
+  "http://a.com/a/b/c?s#h"
+);
