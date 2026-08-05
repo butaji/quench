78e2a22516dde6d83fc38a7091d8ef507c41c497
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 09:14:17 -0500

    Normalize double-slash parent resolution

---
+++ tests/node-compat/stage-1538/url-resolve-double-slash-parent.js
@@ -0,0 +1,8 @@
+const assert = require("node:assert");
+const url = require("node:url");
+
+assert.strictEqual(
+  url.resolve("fred:///s//a/b/c", "../../../g"),
+  "fred:///s/g"
+);
+console.log("double-slash parent resolution passed");
