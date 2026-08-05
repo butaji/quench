c4535a4eec806b00611d2e5db5e8b517bbbda92b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-05 17:04:35 -0500

    Preserve URL query fragment delimiters

---
+++ tests/node-compat/stage-1690/url-query-fragment-delimiters.js
@@ -0,0 +1,4 @@
+const url = new URL("http://foo/path;a??e#f#g");
+if (url.href !== "http://foo/path;a??e#f#g")
+  throw new Error(`Unexpected URL: ${url.href}`);
+console.log("URL query and fragment delimiters are preserved");
