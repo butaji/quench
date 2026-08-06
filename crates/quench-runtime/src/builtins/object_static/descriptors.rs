0036f5b3040c24de07b0a7630fab6e40464cb1a0
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-06 05:09:41 -0500

    fix AggregateError length descriptor

--- crates/quench-runtime/src/builtins/object_static/descriptors.rs
+++ crates/quench-runtime/src/builtins/object_static/descriptors.rs
@@ -1316,10 +1316,11 @@ pub fn get_native_constructor_property_descriptor(
     // Check for custom static methods first
     if let Some(value) = nc.get_static_method(prop) {
         let immutable = nc.is_non_deletable_static_method(prop);
+        let length = prop == "length" && nc.name() == "AggregateError";
         return Ok(make_descriptor_value(
             PropertyFlags {
                 value: Some(value),
-                writable: !immutable,
+                writable: !immutable && !length,
                 enumerable: false,
                 configurable: !immutable,
             },
