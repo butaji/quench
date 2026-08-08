10b07302f907c6b2ed52a27457a59371bf82e448
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 06:57:28 -0500

    Suppress late dgram lookup errors

---
+++ tests/node-compat/stage-2298/dgram-close-before-lookup-error.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const dgram = require("dgram");
+
+let lookupCallback;
+const socket = dgram.createSocket({
+  type: "udp4",
+  lookup(_host, _family, callback) {
+    lookupCallback = callback;
+  }
+});
+let errors = 0;
+socket.on("error", () => errors++);
+socket.bind(12345, "localhost");
+socket.close();
+setImmediate(() => {
+  lookupCallback(new Error("late lookup failure"));
+  setImmediate(() => {
+    assert.strictEqual(errors, 0);
+    console.log("dgram close-before-lookup error passed");
+  });
+});
