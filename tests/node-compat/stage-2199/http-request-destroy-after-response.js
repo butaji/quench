d4432ddc4b9ddaef754ae11d769d2c34c40af62d
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 01:53:27 -0500

    Complete HTTP abort destroy lifecycle

---
+++ tests/node-compat/stage-2199/http-request-destroy-after-response.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const http = require("http");
+
+const server = http.createServer((_request, response) => {
+  response.end("hello");
+});
+
+server.listen(0, () => {
+  const request = http.get({ port: server.address().port }, (response) => {
+    response.on("data", () => {
+      request.destroy();
+      assert.strictEqual(request.aborted, false);
+      assert.strictEqual(request.destroyed, true);
+      server.close();
+    });
+  });
+  request.on("error", () =>
+    assert.fail("destroy after response should not emit error")
+  );
+});
