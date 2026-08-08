e43726314ef2424b56d4228ee537b52c0f8b141d
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-07 23:53:41 -0500

    Implement Duplex toWeb bridge

---
+++ tests/node-compat/stage-2151/stream-duplex-to-web.js
@@ -0,0 +1,22 @@
+const assert = require("assert");
+const { Duplex } = require("stream");
+
+const duplex = Duplex({
+  read() {
+    this.push(Buffer.from("hello"));
+    this.push(null);
+  },
+  write(chunk, _encoding, callback) {
+    assert.deepStrictEqual(chunk, Buffer.from("world"));
+    callback();
+  }
+});
+const { readable, writable } = Duplex.toWeb(duplex);
+writable.getWriter().write(Buffer.from("world"));
+readable
+  .getReader()
+  .read()
+  .then((result) => {
+    assert.deepStrictEqual(Buffer.from(result.value), Buffer.from("hello"));
+    console.log("stream duplex to web pass");
+  });
