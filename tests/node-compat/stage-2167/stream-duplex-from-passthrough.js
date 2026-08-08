3ecbcd265e2c8800ddfe2190333b1402336dfc5e
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 00:05:31 -0500

    Cover Duplex PassThrough pipelines

---
+++ tests/node-compat/stage-2167/stream-duplex-from-passthrough.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+const { Duplex, PassThrough, Readable } = require("stream");
+
+const through = new PassThrough({ objectMode: true });
+let result = "";
+const duplex = Readable.from(["foo", "bar"], { objectMode: true }).pipe(
+  Duplex.from({ readable: through, writable: through })
+);
+duplex.on("data", (data) => {
+  duplex.pause();
+  setImmediate(() => duplex.resume());
+  result += data;
+});
+duplex.on("end", () => {
+  assert.strictEqual(result, "foobar");
+  console.log("stream duplex passthrough pass");
+});
+duplex.on("close", () => assert.strictEqual(result, "foobar"));
