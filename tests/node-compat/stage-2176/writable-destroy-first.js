799af7d6be2f035eeab54176215b1bc0127347fa
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 00:35:20 -0500

    Cover writable destroy lifecycle basics

---
+++ tests/node-compat/stage-2176/writable-destroy-first.js
@@ -0,0 +1,22 @@
+const { Writable } = require("stream");
+
+const first = new Writable({
+  write(_chunk, _encoding, callback) {
+    callback();
+  }
+});
+first.on("close", () => {});
+first.destroy();
+if (!first.destroyed) throw new Error("first was not destroyed");
+
+const second = new Writable({
+  write(_chunk, _encoding, callback) {
+    this.destroy(new Error("asd"));
+    callback();
+  }
+});
+second.on("error", (error) => {
+  if (error.message !== "asd") throw new Error("wrong error");
+});
+second.end("asd");
+if (!second.destroyed) throw new Error("second was not destroyed");
