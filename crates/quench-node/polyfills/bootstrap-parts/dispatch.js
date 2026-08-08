67effa869a6e9ce0d557399f0a363e4349027393
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 11:47:21 -0500

    Add virtual file handle base module

--- crates/quench-node/polyfills/bootstrap-parts/dispatch.js
+++ crates/quench-node/polyfills/bootstrap-parts/dispatch.js
@@ -39,6 +39,102 @@ globalThis.require = (specifier) => {
       isAbsolutePath: path.isAbsolute
     };
   }
+  if (name === "internal/vfs/file_handle") {
+    class VirtualFileHandle {
+      constructor(path, flags = "r", mode = 0o666) {
+        this.path = path;
+        this.flags = flags;
+        this.mode = mode;
+        this.position = 0;
+        this.closed = false;
+      }
+      __check() {
+        if (this.closed) {
+          const error = new Error("file handle is closed");
+          error.code = "EBADF";
+          throw error;
+        }
+      }
+      __stub() {
+        this.__check();
+        const error = new Error("Method not implemented");
+        error.code = "ERR_METHOD_NOT_IMPLEMENTED";
+        throw error;
+      }
+      readSync() {
+        return this.__stub();
+      }
+      writeSync() {
+        return this.__stub();
+      }
+      readFileSync() {
+        return this.__stub();
+      }
+      writeFileSync() {
+        return this.__stub();
+      }
+      statSync() {
+        return this.__stub();
+      }
+      truncateSync() {
+        return this.__stub();
+      }
+      read() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      write() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      readFile() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      writeFile() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      stat() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      truncate() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      readv() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      writev() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      appendFile() {
+        return Promise.reject().catch(() => this.__stub());
+      }
+      readableWebStream() {
+        return this.__stub();
+      }
+      readLines() {
+        return this.__stub();
+      }
+      createReadStream() {
+        return this.__stub();
+      }
+      createWriteStream() {
+        return this.__stub();
+      }
+      async chmod() {}
+      async chown() {}
+      async utimes() {}
+      async datasync() {}
+      async sync() {}
+      closeSync() {
+        this.closed = true;
+      }
+      async close() {
+        this.closed = true;
+      }
+    }
+    Symbol.asyncDispose ||= Symbol("Symbol.asyncDispose");
+    VirtualFileHandle.prototype[Symbol.asyncDispose] =
+      VirtualFileHandle.prototype.close;
+    return { VirtualFileHandle };
+  }
   if (name === "worker_threads") {
     return { isMainThread: true, MessageChannel, MessagePort };
   }
