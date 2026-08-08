7709ab53c2a1724d3de28541b707e53ff97b80a3
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 07:05:23 -0500

    Reject connected dgram destination overload

---
+++ tests/node-compat/stage-2303/dgram-connected-port-rejection.js
@@ -0,0 +1,13 @@
+const assert = require("assert");
+const dgram = require("dgram");
+
+const socket = dgram.createSocket("udp4");
+socket.connect(12345, () => {
+  assert.throws(
+    () => socket.send(Buffer.from("x"), 1234, "127.0.0.1", () => {}),
+    { code: "ERR_SOCKET_DGRAM_IS_CONNECTED", message: "Already connected" }
+  );
+  socket.send(Buffer.from("hello"), 0, 5, () => {});
+  socket.close();
+  console.log("dgram connected port rejection passed");
+});
