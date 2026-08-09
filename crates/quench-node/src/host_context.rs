88c2fc8fdac8a59ceb397b814591a21978c299a5
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-09 04:50:00 -0500

    Split legacy URL helper surface

--- crates/quench-node/src/host_context.rs
+++ crates/quench-node/src/host_context.rs
@@ -824,6 +824,7 @@ macro_rules! run_host_context {
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-11-tail.js").as_bytes())?;
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-11-final.js").as_bytes())?;
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-12.js").as_bytes())?;
+        ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-12-tail.js").as_bytes())?;
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-13.js").as_bytes())?;
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-14.js").as_bytes())?;
         ctx.eval::<(), _>(include_str!("../polyfills/post-bootstrap/module-surface-15.js").as_bytes())?;
