0675cddccf7a213b8dfc328d7744f07054ae4257
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 19:36:12 -0500

    Order datagram connect listeners

---
+++ tests/node-compat/stage-2484/dgram-connect-listener-order.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const dgram = require("dgram");
+
+const socket = dgram.createSocket("udp4");
+const events = [];
+socket.once("connect", () => events.push("listener"));
+socket.connect(12345, () => events.push("callback"));
+
+queueMicrotask(() => {
+  assert.deepStrictEqual(events, ["listener", "callback"]);
+  assert.deepStrictEqual(socket.remoteAddress(), {
+    address: "127.0.0.1",
+    family: "IPv4",
+    port: 12345
+  });
+  socket.close();
+});
