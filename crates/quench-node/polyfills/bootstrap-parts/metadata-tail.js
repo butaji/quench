d28b152a5c40009187fc702ec2f84766489b87ac
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-09 04:52:56 -0500

    Split metadata readlink surface

---
+++ crates/quench-node/polyfills/bootstrap-parts/metadata-tail.js
@@ -0,0 +1,26 @@
+globalThis.__nodeFs.readlink = (value, options, callback) => {
+  if (typeof options === "function") {
+    callback = options;
+    options = undefined;
+  }
+  if (typeof callback !== "function") {
+    throw new TypeError('The "callback" argument must be of type function');
+  }
+  const encoding =
+    typeof options === "string" ? options : options && options.encoding;
+  if (encoding !== undefined && !NodeBuffer.isEncoding(encoding)) {
+    const error = new TypeError(
+      `The argument 'encoding' is invalid. Received '${encoding}'`
+    );
+    error.code = "ERR_INVALID_ARG_VALUE";
+    throw error;
+  }
+  const path = nodeFsPath(value);
+  queueMicrotask(() => {
+    try {
+      callback(null, globalThis.__nodeFs.readlinkSync(path, options));
+    } catch (error) {
+      callback(error);
+    }
+  });
+};
