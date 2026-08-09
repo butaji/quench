b76f327718e783f099bdf2110aa38c3b2f1f4857
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-09 09:26:52 -0500

    Split readFile validation helpers

--- crates/quench-node/polyfills/bootstrap-parts/read-file.js
+++ crates/quench-node/polyfills/bootstrap-parts/read-file.js
@@ -23,31 +23,9 @@ const __validateReadFileURL = (value) => {
     throw error;
   }
 };
-globalThis.__nodeFs.readFile = (value, options, callback) => {
-  if (typeof options === "function") {
-    callback = options;
-    options = undefined;
-  }
-  if (typeof callback !== "function") {
-    const error = new TypeError(
-      'The "callback" argument must be of type function'
-    );
-    error.code = "ERR_INVALID_ARG_TYPE";
-    throw error;
-  }
-  const encoding =
-    typeof options === "string" ? options : options && options.encoding;
-  if (encoding !== undefined && !NodeBuffer.isEncoding(encoding)) {
-    const error = new TypeError(
-      `The argument 'encoding' is invalid. Received '${encoding}'`
-    );
-    error.code = "ERR_INVALID_ARG_VALUE";
-    throw error;
-  }
+const __readFileValidatePath = (value) => {
   if (typeof value === "function") {
-    const error = new TypeError(
-      'The "path" argument must be of type string or an instance of Buffer or URL'
-    );
+    const error = new TypeError('The "path" argument must be a path');
     error.code = "ERR_INVALID_ARG_TYPE";
     throw error;
   }
@@ -56,10 +34,33 @@ globalThis.__nodeFs.readFile = (value, options, callback) => {
     error.code = "ERR_INVALID_URL_SCHEME";
     throw error;
   }
-  if (value instanceof globalThis.__nodeURL) __validateReadFileURL(value);
   if (value instanceof globalThis.__nodeURL) {
+    __validateReadFileURL(value);
     globalThis.__nodeUrlModule.fileURLToPath(value);
   }
+};
+const __readFileValidateOptions = (options) => {
+  const encoding = typeof options === "string" ? options : options?.encoding;
+  if (encoding !== undefined && !NodeBuffer.isEncoding(encoding)) {
+    const error = new TypeError(`The argument 'encoding' is invalid. Received '${encoding}'`);
+    error.code = "ERR_INVALID_ARG_VALUE";
+    throw error;
+  }
+};
+globalThis.__nodeFs.readFile = (value, options, callback) => {
+  if (typeof options === "function") {
+    callback = options;
+    options = undefined;
+  }
+  if (typeof callback !== "function") {
+    const error = new TypeError(
+      'The "callback" argument must be of type function',
+    );
+    error.code = "ERR_INVALID_ARG_TYPE";
+    throw error;
+  }
+  __readFileValidateOptions(options);
+  __readFileValidatePath(value);
   if (options && options.signal !== undefined) {
     if (!(options.signal instanceof NodeAbortSignal)) {
       const error = new TypeError('The "signal" option must be an AbortSignal');
