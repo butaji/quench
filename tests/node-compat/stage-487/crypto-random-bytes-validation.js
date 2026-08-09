33e877dbfc4c059ff97999d531870bdf4e71a8db
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-08 23:23:01 -0500

    Align randomBytes fractional sizes

--- tests/node-compat/stage-487/crypto-random-bytes-validation.js
+++ tests/node-compat/stage-487/crypto-random-bytes-validation.js
@@ -1,12 +1,6 @@
 const { randomBytes } = require("crypto");
 
-for (
-  const call of [
-    () => randomBytes(-1),
-    () => randomBytes(1.5),
-    () => randomBytes(1, 123),
-  ]
-) {
+for (const call of [() => randomBytes(-1), () => randomBytes(1, 123)]) {
   let error;
   try {
     call();
@@ -14,6 +8,10 @@ for (
     error = caught;
   }
   if (!error) throw new Error("randomBytes accepted invalid arguments");
+}
+
+if (randomBytes(1.5).length !== 1) {
+  throw new Error("randomBytes did not truncate fractional size");
 }
 
 console.log("crypto random bytes validation passed");
