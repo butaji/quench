3e15be525dfb902f66fedd44378293c98c1fa6fe
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-10 03:11:23 -0500

    wire Uint8Array globals and construction

--- crates/quench-runtime/src/globals.rs
+++ crates/quench-runtime/src/globals.rs
@@ -50,6 +50,7 @@ fn builtin(name: &str) -> Option<crate::ops::Builtin> {
         "Float64Array" => Some(crate::ops::Builtin::Float64Array),
         "Float32Array" => Some(crate::ops::Builtin::Float32Array),
         "Int8Array" => Some(crate::ops::Builtin::Int8Array),
+        "Uint8Array" => Some(crate::ops::Builtin::Uint8Array),
         "DataView" => Some(crate::ops::Builtin::DataView),
         "Boolean" => Some(crate::ops::Builtin::Boolean),
         "eval" => Some(crate::ops::Builtin::Eval),
