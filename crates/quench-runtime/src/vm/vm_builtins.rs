dbe658a8e92c003472efeaa73a8daa1f18f9d276
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-11 08:23:21 -0500

    Return strings from Date calls

--- crates/quench-runtime/src/vm/vm_builtins.rs
+++ crates/quench-runtime/src/vm/vm_builtins.rs
@@ -10,7 +10,7 @@ fn early_dispatch(
         .or_else(|| crate::intl::tolocale::dispatch(builtin, receiver, arguments))
         .or_else(|| crate::collections::execute_builtin(builtin, receiver, arguments))
         .or_else(|| crate::promise::execute_builtin(builtin, receiver, arguments))
-        .or_else(|| crate::date::execute(builtin, receiver, arguments))
+        .or_else(|| (builtin != Builtin::Date).then(|| crate::date::execute(builtin, receiver, arguments))?)
 }
 
 fn is_function_builtin(builtin: Builtin) -> bool {
