89c0ea3678acb200f59d487f218dae3ac540d39c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-14 18:49:02 -0500

    expose SharedArrayBuffer grow method

--- crates/quench-runtime/src/vm/vm_properties.rs
+++ crates/quench-runtime/src/vm/vm_properties.rs
@@ -520,6 +520,9 @@ fn promise_property(value: &Value, key: &str) -> Value {
     }))
 }
 fn array_buffer_property(buffer: &crate::value::ArrayBufferData, key: &str) -> Value {
+    if key == "grow" && buffer.shared {
+        return Value::Builtin(Builtin::SharedArrayBufferGrow);
+    }
     match key {
         "byteLength" => Value::Number(buffer.byte_length() as f64),
         "maxByteLength" => {
