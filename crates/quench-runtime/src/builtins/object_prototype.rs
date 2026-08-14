48ac8fafab08e0f810ae55e7fc9181aa5a862857
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-14 18:45:24 -0500

    Use Object prototype for Intl prototypes

--- crates/quench-runtime/src/builtins/object_prototype.rs
+++ crates/quench-runtime/src/builtins/object_prototype.rs
@@ -291,6 +291,15 @@ pub(crate) fn is_intrinsic_prototype(builtin: Builtin) -> bool {
             | Builtin::WeakRefPrototype
             | Builtin::FinalizationRegistryPrototype
             | Builtin::BigIntPrototype
+            | Builtin::IntlCollatorPrototype
+            | Builtin::IntlDateTimeFormatPrototype
+            | Builtin::IntlDisplayNamesPrototype
+            | Builtin::IntlListFormatPrototype
+            | Builtin::IntlLocalePrototype
+            | Builtin::IntlNumberFormatPrototype
+            | Builtin::IntlPluralRulesPrototype
+            | Builtin::IntlRelativeTimeFormatPrototype
+            | Builtin::IntlSegmenterPrototype
     )
 }
 
