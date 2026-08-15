f001b93753359773e61d715647d55920e63942b7
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 09:51:53 -0500

    share async generator prototype identity

--- crates/quench-runtime/src/functions_reduction_tail.rs
+++ crates/quench-runtime/src/functions_reduction_tail.rs
@@ -37,11 +37,14 @@ fn attach_generator_prototype(function: &std::rc::Rc<crate::value::FunctionValue
     } else {
         crate::ops::Builtin::GeneratorFunctionPrototype
     };
-    let generator =
+    let generator = if function.is_async {
+        crate::builtins::async_generator_prototype()
+    } else {
         crate::value::Value::Object(std::rc::Rc::new(crate::value::ObjectData::new(vec![(
             "\0prototype".to_string(),
             crate::value::Value::Builtin(crate::ops::Builtin::ObjectPrototype),
-        )])));
+        )])))
+    };
     let function_prototype =
         crate::value::Value::Object(std::rc::Rc::new(crate::value::ObjectData::new(vec![
             ("prototype".to_string(), generator.clone()),
