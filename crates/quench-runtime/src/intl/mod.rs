489ba895b822e61f3251ef9900ee4a3d69a54a85
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 05:31:46 -0500

    Split Intl prototype property dispatch

--- crates/quench-runtime/src/intl/mod.rs
+++ crates/quench-runtime/src/intl/mod.rs
@@ -86,67 +86,7 @@ fn constructor_property(builtin: Builtin, key: &str) -> Option<Builtin> {
     })
 }
 
-fn prototype_property(builtin: Builtin, key: &str) -> Option<Builtin> {
-    Some(match (builtin, key) {
-        (Builtin::IntlLocalePrototype, "toString") => Builtin::IntlLocaleToString,
-        (Builtin::IntlLocalePrototype, "maximize") => Builtin::IntlLocaleMaximize,
-        (Builtin::IntlLocalePrototype, "minimize") => Builtin::IntlLocaleMinimize,
-        (Builtin::IntlLocalePrototype, "getCalendars") => Builtin::IntlLocaleGetCalendars,
-        (Builtin::IntlLocalePrototype, "getCollations") => Builtin::IntlLocaleGetCollations,
-        (Builtin::IntlLocalePrototype, "getHourCycles") => Builtin::IntlLocaleGetHourCycles,
-        (Builtin::IntlLocalePrototype, "getNumberingSystems") => {
-            Builtin::IntlLocaleGetNumberingSystems
-        }
-        (Builtin::IntlLocalePrototype, "getTimeZones") => Builtin::IntlLocaleGetTimeZones,
-        (Builtin::IntlLocalePrototype, "getTextInfo") => Builtin::IntlLocaleGetTextInfo,
-        (Builtin::IntlLocalePrototype, "getWeekInfo") => Builtin::IntlLocaleGetWeekInfo,
-        (Builtin::IntlLocalePrototype, "baseName") => Builtin::IntlLocaleBaseNameGetter,
-        (Builtin::IntlLocalePrototype, "calendar") => Builtin::IntlLocaleCalendarGetter,
-        (Builtin::IntlLocalePrototype, "caseFirst") => Builtin::IntlLocaleCaseFirstGetter,
-        (Builtin::IntlLocalePrototype, "collation") => Builtin::IntlLocaleCollationGetter,
-        (Builtin::IntlLocalePrototype, "firstDayOfWeek") => Builtin::IntlLocaleFirstDayOfWeekGetter,
-        (Builtin::IntlLocalePrototype, "hourCycle") => Builtin::IntlLocaleHourCycleGetter,
-        (Builtin::IntlLocalePrototype, "language") => Builtin::IntlLocaleLanguageGetter,
-        (Builtin::IntlLocalePrototype, "numberingSystem") => {
-            Builtin::IntlLocaleNumberingSystemGetter
-        }
-        (Builtin::IntlLocalePrototype, "numeric") => Builtin::IntlLocaleNumericGetter,
-        (Builtin::IntlLocalePrototype, "region") => Builtin::IntlLocaleRegionGetter,
-        (Builtin::IntlLocalePrototype, "script") => Builtin::IntlLocaleScriptGetter,
-        (Builtin::IntlLocalePrototype, "textInfo") => Builtin::IntlLocaleTextInfoGetter,
-        (Builtin::IntlLocalePrototype, "variants") => Builtin::IntlLocaleVariantsGetter,
-        (Builtin::IntlNumberFormatPrototype, "format") => Builtin::IntlNumberFormatFormat,
-        (Builtin::IntlNumberFormatPrototype, "formatToParts") => {
-            Builtin::IntlNumberFormatFormatToParts
-        }
-        (Builtin::IntlNumberFormatPrototype, "formatRange") => Builtin::IntlNumberFormatFormatRange,
-        (Builtin::IntlNumberFormatPrototype, "formatRangeToParts") => {
-            Builtin::IntlNumberFormatFormatRangeToParts
-        }
-        (Builtin::IntlNumberFormatPrototype, "resolvedOptions") => {
-            Builtin::IntlNumberFormatResolvedOptions
-        }
-        (Builtin::IntlDateTimeFormatPrototype, "format") => Builtin::IntlDateTimeFormatFormat,
-        (Builtin::IntlDateTimeFormatPrototype, "formatToParts") => {
-            Builtin::IntlDateTimeFormatFormatToParts
-        }
-        (Builtin::IntlDateTimeFormatPrototype, "formatRange") => {
-            Builtin::IntlDateTimeFormatFormatRange
-        }
-        (Builtin::IntlDateTimeFormatPrototype, "formatRangeToParts") => {
-            Builtin::IntlDateTimeFormatFormatRangeToParts
-        }
-        (Builtin::IntlDateTimeFormatPrototype, "resolvedOptions") => {
-            Builtin::IntlDateTimeFormatResolvedOptions
-        }
-        (Builtin::IntlPluralRulesPrototype, "select") => Builtin::IntlPluralRulesSelect,
-        (Builtin::IntlPluralRulesPrototype, "resolvedOptions") => {
-            Builtin::IntlPluralRulesResolvedOptions
-        }
-        _ => return None,
-    })
-}
-
+include!("prototype_property.rs");
 /// Dispatch a builtin call received with a receiver.
 pub(crate) fn execute(
     builtin: Builtin,
