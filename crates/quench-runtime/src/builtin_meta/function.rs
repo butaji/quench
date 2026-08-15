680e9e31db48115ac0f21ba5b8671b76dfa2550b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 13:26:00 -0500

    correct Function.prototype.apply length

--- crates/quench-runtime/src/builtin_meta/function.rs
+++ crates/quench-runtime/src/builtin_meta/function.rs
@@ -15,7 +15,8 @@ pub const fn fn_name(b: Builtin) -> Option<&'static str> {
 pub const fn fn_len(b: Builtin) -> Option<f64> {
     match b {
         Builtin::ThrowTypeError => Some(0.0),
-        Builtin::FunctionCall | Builtin::FunctionApply | Builtin::FunctionBind => Some(1.0),
+        Builtin::FunctionCall | Builtin::FunctionBind => Some(1.0),
+        Builtin::FunctionApply => Some(2.0),
         _ => None,
     }
 }
