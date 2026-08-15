cf1d462f9f502d175ed4f13418b199b372d5c08c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 05:45:40 -0500

    split relative time dispatch

--- crates/quench-runtime/src/intl/relative.rs
+++ crates/quench-runtime/src/intl/relative.rs
@@ -500,21 +500,4 @@ fn relative_resolved_options(
     ]))
 }
 
-fn receiver_slots(receiver: Option<&Value>) -> Result<Vec<(String, Value)>, VmError> {
-    super::intl_slots(receiver)
-}
-pub(crate) fn dispatch(
-    builtin: crate::ops::Builtin,
-    arguments: &[Value],
-    receiver: Option<&Value>,
-) -> Option<Result<Value, VmError>> {
-    match builtin {
-        crate::ops::Builtin::IntlRelativeTimeFormat => Some(construct(arguments)),
-        crate::ops::Builtin::IntlRelativeTimeFormatFormat
-        | crate::ops::Builtin::IntlRelativeTimeFormatFormatToParts
-        | crate::ops::Builtin::IntlRelativeTimeFormatResolvedOptions => {
-            Some(prototype_method(builtin, arguments, receiver))
-        }
-        _ => None,
-    }
-}
+include!("relative_dispatch.rs");
