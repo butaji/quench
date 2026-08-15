442be5f50e94cd68cc5a374ae6a6721c0781837b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-14 19:59:01 -0500

    format runtime sources

--- crates/quench-runtime/src/construct.rs
+++ crates/quench-runtime/src/construct.rs
@@ -518,9 +518,10 @@ fn construct_error(
         ),
         (
             "\0prototype".to_string(),
-            Value::Builtin(crate::builtin_meta::instance_prototype(*builtin).unwrap_or(
-                crate::ops::Builtin::ErrorPrototype,
-            )),
+            Value::Builtin(
+                crate::builtin_meta::instance_prototype(*builtin)
+                    .unwrap_or(crate::ops::Builtin::ErrorPrototype),
+            ),
         ),
     ];
 
