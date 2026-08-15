dc7ef821cddbe58936def8905d98c0676cbf7098
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 09:02:31 -0500

    fix normal completion of array binding iterators

--- crates/quench-runtime/src/binding_patterns.rs
+++ crates/quench-runtime/src/binding_patterns.rs
@@ -354,7 +354,7 @@ fn bind_array(
     ops.push(Op::IteratorBinding {
         iterator,
         body: crate::machine::FunctionCode::from_ops(body),
-        close_normal: true,
+        close_normal: false,
     });
     Some(())
 }
