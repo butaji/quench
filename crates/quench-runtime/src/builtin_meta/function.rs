d7bab9b4d93b2f0d732aa305f8de9e66ac0ca284
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-14 20:43:22 -0500

    restore Function prototype toString metadata

--- crates/quench-runtime/src/builtin_meta/function.rs
+++ crates/quench-runtime/src/builtin_meta/function.rs
@@ -8,6 +8,7 @@ pub const fn fn_name(b: Builtin) -> Option<&'static str> {
         Builtin::FunctionCall => Some("Function.prototype.call"),
         Builtin::FunctionApply => Some("Function.prototype.apply"),
         Builtin::FunctionBind => Some("Function.prototype.bind"),
+        Builtin::FunctionPrototypeToString => Some("Function.prototype.toString"),
         Builtin::FunctionPrototypeHasInstance => Some("Function.prototype[@@hasInstance]"),
         _ => None,
     }
@@ -20,6 +21,7 @@ pub const fn fn_len(b: Builtin) -> Option<f64> {
             Some(1.0)
         }
         Builtin::FunctionApply => Some(2.0),
+        Builtin::FunctionPrototypeToString => Some(0.0),
         _ => None,
     }
 }
@@ -30,6 +32,7 @@ pub const fn short_name(b: Builtin) -> Option<&'static str> {
         Builtin::FunctionCall => Some("call"),
         Builtin::FunctionApply => Some("apply"),
         Builtin::FunctionBind => Some("bind"),
+        Builtin::FunctionPrototypeToString => Some("toString"),
         Builtin::FunctionPrototypeHasInstance => Some("[Symbol.hasInstance]"),
         _ => None,
     }
