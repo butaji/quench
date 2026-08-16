b8bce37090ed3321574f5a775498db08d22f26a6
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-16 02:13:39 -0500

    Implement Temporal Duration field accessors

--- crates/quench-runtime/src/builtins/object_descriptor_core.rs
+++ crates/quench-runtime/src/builtins/object_descriptor_core.rs
@@ -240,6 +240,16 @@ fn intrinsic_getter_extended(builtin: Builtin, key: &str) -> Option<Builtin> {
 fn intrinsic_getter_tail(builtin: Builtin, key: &str) -> Option<Builtin> {
     use Builtin::*;
     Some(match (builtin, key) {
+        (TemporalDurationPrototype, "years") => TemporalDurationYearsGetter,
+        (TemporalDurationPrototype, "months") => TemporalDurationMonthsGetter,
+        (TemporalDurationPrototype, "weeks") => TemporalDurationWeeksGetter,
+        (TemporalDurationPrototype, "days") => TemporalDurationDaysGetter,
+        (TemporalDurationPrototype, "hours") => TemporalDurationHoursGetter,
+        (TemporalDurationPrototype, "minutes") => TemporalDurationMinutesGetter,
+        (TemporalDurationPrototype, "seconds") => TemporalDurationSecondsGetter,
+        (TemporalDurationPrototype, "milliseconds") => TemporalDurationMillisecondsGetter,
+        (TemporalDurationPrototype, "microseconds") => TemporalDurationMicrosecondsGetter,
+        (TemporalDurationPrototype, "nanoseconds") => TemporalDurationNanosecondsGetter,
         (IntlDateTimeFormatPrototype, "format") => IntlDateTimeFormatFormatGetter,
         (IntlCollatorPrototype, "compare") => IntlCollatorCompareGetter,
         (IntlLocalePrototype, "baseName") => IntlLocaleBaseNameGetter,
