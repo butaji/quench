10a00562ab175ec1b7e13d8639025ed1c9b94662
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-16 10:38:13 -0500

    Implement String.prototype.fixed

--- crates/quench-runtime/src/ops_builtin_enum.rs
+++ crates/quench-runtime/src/ops_builtin_enum.rs
@@ -46,7 +46,7 @@ pub enum Builtin {
     ArraySlice, ArrayConcat,
     ArrayFlat, ArrayFlatMap,
     ArrayAt, ArraySort,
-    ArrayToReversed, StringAnchor, StringBig, StringBold, StringIncludes,
+    ArrayToReversed, StringAnchor, StringBig, StringBold, StringFixed, StringIncludes,
     StringIsWellFormed, StringToWellFormed,
     StringStartsWith, StringEndsWith,
     StringAt, StringRepeat,
