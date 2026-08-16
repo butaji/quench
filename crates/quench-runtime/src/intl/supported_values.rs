1f639af947d3aa278d883c3cbfcd28b2b3561a6b
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-16 02:06:09 -0500

    Include primary noncontinental time zones

--- crates/quench-runtime/src/intl/supported_values.rs
+++ crates/quench-runtime/src/intl/supported_values.rs
@@ -72,7 +72,35 @@ pub(crate) const NUMBERING_SYSTEMS: &[&str] = &[
 ];
 
 pub(crate) fn supported_time_zones() -> Vec<Value> {
-    strings(&["UTC"])
+    strings(&[
+        "Etc/GMT+1",
+        "Etc/GMT+10",
+        "Etc/GMT+11",
+        "Etc/GMT+12",
+        "Etc/GMT+2",
+        "Etc/GMT+3",
+        "Etc/GMT+4",
+        "Etc/GMT+5",
+        "Etc/GMT+6",
+        "Etc/GMT+7",
+        "Etc/GMT+8",
+        "Etc/GMT+9",
+        "Etc/GMT-1",
+        "Etc/GMT-10",
+        "Etc/GMT-11",
+        "Etc/GMT-12",
+        "Etc/GMT-13",
+        "Etc/GMT-14",
+        "Etc/GMT-2",
+        "Etc/GMT-3",
+        "Etc/GMT-4",
+        "Etc/GMT-5",
+        "Etc/GMT-6",
+        "Etc/GMT-7",
+        "Etc/GMT-8",
+        "Etc/GMT-9",
+        "UTC",
+    ])
 }
 
 pub(crate) const UNITS: &[&str] = &[
