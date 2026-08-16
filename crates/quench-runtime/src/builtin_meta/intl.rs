7af83f5a957fb50faaa1189b8f71428a1c1e8913
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 19:57:40 -0500

    Set supportedValuesOf builtin name

--- crates/quench-runtime/src/builtin_meta/intl.rs
+++ crates/quench-runtime/src/builtin_meta/intl.rs
@@ -152,6 +152,7 @@ const fn intl_fn_len_tail(b: Builtin) -> Option<f64> {
 
 const fn intl_short_name(b: Builtin) -> Option<&'static str> {
     match b {
+        Builtin::IntlSupportedValuesOf => Some("supportedValuesOf"),
         Builtin::IntlDisplayNames => Some("DisplayNames"),
         Builtin::IntlLocale => Some("Locale"),
         Builtin::IntlRelativeTimeFormat => Some("RelativeTimeFormat"),
