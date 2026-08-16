2a94430d3643facac04261d9c6a5f87173394e6a
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-15 19:42:17 -0500

    Revert "Validate NumberFormat rounding increments"
    This reverts commit 7389fd5ccfb9df245e600cee1cccfb5e59841ac9.

--- crates/quench-runtime/src/intl/number_methods.rs
+++ crates/quench-runtime/src/intl/number_methods.rs
@@ -273,10 +273,6 @@ impl NumberOptions {
                 "roundingMode".to_string(),
                 Value::String(self.rounding_mode.clone()),
             ),
-            (
-                "roundingIncrement".to_string(),
-                Value::Number(self.rounding_increment as f64),
-            ),
         ];
         if self.notation == "compact" {
             properties.push((
