52a4380af859c336ee2a0c92f15de046a25b7f90
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-18 19:03:46 -0500

    os: cpus() returns Node-shaped array of CPU objects
    Each entry now has { model, speed, times: { user, nice, sys,
    idle, irq } } matching the real Node API. The compat test asserts
    the shape (typeof string / number / object).

--- crates/quench-node-test/node-tests/test-os.js
+++ crates/quench-node-test/node-tests/test-os.js
@@ -1,6 +1,14 @@
+// Node compat: os module basics.
 const os = require('node:os');
-console.log('totalmem:', os.totalmem());
-console.log('freemem:', os.freemem());
-console.log('cpus:', os.cpus().length);
-console.log('uptime:', os.uptime());
-console.log('loadavg:', os.loadavg()[0], os.loadavg()[1], os.loadavg()[2]);
+const totalmem = os.totalmem();
+if (!(totalmem > 0)) throw new Error('totalmem=' + totalmem);
+const freemem = os.freemem();
+if (!(freemem > 0)) throw new Error('freemem=' + freemem);
+const cpus = os.cpus();
+if (!(cpus.length > 0)) throw new Error('cpus=' + cpus.length);
+if (typeof cpus[0].model !== 'string') throw new Error('model=' + typeof cpus[0].model);
+if (typeof cpus[0].speed !== 'number') throw new Error('speed=' + typeof cpus[0].speed);
+const times = cpus[0].times;
+if (typeof times.user !== 'number') throw new Error('times.user=' + typeof times.user);
+if (typeof times.sys !== 'number') throw new Error('times.sys=' + typeof times.sys);
+console.log('os: ' + totalmem + ' ' + cpus.length + ' ' + freemem);
