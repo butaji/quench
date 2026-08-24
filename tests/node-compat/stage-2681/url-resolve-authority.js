8485346f749c8f632906a83968d41ce177379980
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-24 02:19:36 -0500

    test: expand URL authority stage

--- tests/node-compat/stage-2681/url-resolve-authority.js
+++ tests/node-compat/stage-2681/url-resolve-authority.js
@@ -4,3 +4,11 @@ assert.strictEqual(
   url.resolve('http://asdf:qwer@www.example.com', 'http://diff:auth@www.example.com'),
   'http://diff:auth@www.example.com/',
 );
+assert.strictEqual(
+  url.resolve('https://user:password@example.org/', 'https://another.host.com/'),
+  'https://another.host.com/',
+);
+assert.strictEqual(
+  url.resolve('https://example.com/foo', 'https://user:password@example.com'),
+  'https://user:password@example.com/foo',
+);
