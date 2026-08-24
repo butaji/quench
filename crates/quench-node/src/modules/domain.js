d2a85e92dd293e403539eca888094bd51c69fc10
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-24 13:30:40 -0500

    preserve domain member and stack identity

--- crates/quench-node/src/modules/domain.js
+++ crates/quench-node/src/modules/domain.js
@@ -6,8 +6,9 @@ function create() {
     enter() {
       if (!d.disposed) {
         var index = stack.indexOf(d);
-        if (index >= 0) stack.splice(index, 1);
-        stack.push(d);
+        if (index >= 0) stack = stack.slice(0, index).concat(stack.slice(index + 1));
+        stack = stack.concat([d]);
+        module.exports._stack = stack;
         module.exports.active = d;
         d.active = true;
       }
@@ -15,13 +16,25 @@ function create() {
     },
     exit() {
       var index = stack.indexOf(d);
-      if (index >= 0) stack.splice(index);
+      if (index >= 0) stack = stack.slice(0, index);
+      module.exports._stack = stack;
       d.active = false;
       module.exports.active = stack.length ? stack[stack.length - 1] : null;
       return d;
     },
-    add(x) { if (!d.disposed && d.members.indexOf(x) < 0) { d.members.push(x); if (x && (typeof x === 'object' || typeof x === 'function')) Object.defineProperty(x, 'domain', { configurable: true, enumerable: false, value: d, writable: true }); } return d; },
-    remove(x) { const i = d.members.indexOf(x); if (i >= 0) d.members.splice(i, 1); if (x && x.domain === d) delete x.domain; return d; },
+    add(x) {
+      if (!d.disposed && d.members.indexOf(x) < 0) {
+        d.members = d.members.concat([x]);
+        if (x && (typeof x === 'object' || typeof x === 'function')) Object.defineProperty(x, 'domain', { configurable: true, enumerable: false, value: d, writable: true });
+      }
+      return d;
+    },
+    remove(x) {
+      const i = d.members.indexOf(x);
+      if (i >= 0) d.members.splice(i, 1);
+      if (x && x.domain === d) delete x.domain;
+      return d;
+    },
     run(fn) { d.enter(); try { return fn(); } catch (e) { if (d._handler) return d._handler(e); throw e; } finally { d.exit(); } },
     bind(fn) { return function() { return d.run(() => fn.apply(this, arguments)); }; },
     intercept(fn) { return d.bind(function() { try { return fn.apply(this, arguments); } catch (e) { if (e && e.domain) delete e.domain; throw e; } }); },
