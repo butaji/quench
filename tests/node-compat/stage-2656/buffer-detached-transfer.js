0f705da85207f1a9a438fabf7fd2d8a490ce3442
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-23 21:49:22 -0500

    isolate immutable markers in captured frames

--- tests/node-compat/stage-2656/buffer-detached-transfer.js
+++ tests/node-compat/stage-2656/buffer-detached-transfer.js
@@ -1,10 +1,30 @@
 "use strict";
 
 const assert = require("assert");
+require("../../node/test/common");
 const { isAscii, isUtf8, Buffer } = require("buffer");
 const { TextEncoder } = require("util");
 
 assert.strictEqual(isAscii(new TextEncoder().encode("hello")), true);
+assert.strictEqual(isAscii(new TextEncoder().encode("ğ")), false);
+for (const input of [
+  undefined,
+  "",
+  "hello",
+  false,
+  true,
+  0,
+  1,
+  0n,
+  1n,
+  Symbol(),
+  () => {},
+  {},
+  [],
+  null,
+]) {
+  assert.throws(() => isAscii(input), { code: "ERR_INVALID_ARG_TYPE" });
+}
 
 const source = new ArrayBuffer(1);
 const view = new Uint8Array(source);
