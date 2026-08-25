9b340f82a766d7e0c0bdd06c17177a4103298b3c
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-24 21:31:32 -0500

    test(node): cover assertion error inheritance

--- tests/node-compat/stage-2731/assert-rust-family.js
+++ tests/node-compat/stage-2731/assert-rust-family.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
+assert.ok(assert.AssertionError.prototype instanceof Error);
 
 assert.deepStrictEqual({ value: [1, 2], nested: { ok: true } }, {
   value: [1, 2], nested: { ok: true },
 });
 assert.notDeepStrictEqual([1, 2], [1, 3]);
 assert.deepStrictEqual(new Uint8Array([1, 2]), new Uint8Array([1, 2]));
+assert.throws(() => assert.deepStrictEqual([1], [2]), {
+  code: 'ERR_ASSERTION',
+  operator: 'deepStrictEqual',
+});
