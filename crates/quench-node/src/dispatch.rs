908c732003c5c8d0415488484e774689e0ab03b9
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-26 15:14:54 -0500

    expose net BlockList capability

--- crates/quench-node/src/dispatch.rs
+++ crates/quench-node/src/dispatch.rs
@@ -385,6 +385,8 @@ const CAP_NET_SOCKET_END: u16 = 0x100B;
 const CAP_NET_SOCKET_DESTROY: u16 = 0x100C;
 const CAP_NET_SOCKET_UNREF: u16 = 2290;
 const CAP_NET_SOCKET_REF: u16 = 2291;
+const CAP_NET_BLOCK_LIST: u16 = 2292;
+const CAP_NET_BLOCK_LIST_ADD_SUBNET: u16 = 2293;
 const CAP_NET_SOCKET_ADDRESS: u16 = 0x100D;
 const CAP_NET_SOCKET_SET_NO_DELAY: u16 = 0x100E;
 const CAP_NET_SOCKET_SET_KEEP_ALIVE: u16 = 0x100F;
@@ -791,6 +793,7 @@ fn network_dispatch(cap: u16) -> Option<CallHandler> {
         CAP_NET_SOCKET_DESTROY => crate::modules::net::socket_destroy,
         CAP_NET_SOCKET_UNREF => crate::modules::net::socket_unref,
         CAP_NET_SOCKET_REF => crate::modules::net::socket_ref,
+        CAP_NET_BLOCK_LIST_ADD_SUBNET => crate::modules::net::block_list_add_subnet,
         CAP_NET_SOCKET_ADDRESS => crate::modules::net::socket_address,
         CAP_NET_SOCKET_SET_NO_DELAY => crate::modules::net::socket_set_no_delay,
         CAP_NET_SOCKET_SET_KEEP_ALIVE => crate::modules::net::socket_set_keep_alive,
@@ -912,6 +915,7 @@ pub fn lookup_construct(cap: u16) -> Option<ConstructHandler> {
         CAP_NET_SERVER => net_create_server,
         CAP_NET_CONNECT => crate::modules::net::socket_construct,
         CAP_NET_SOCKET => crate::modules::net::socket_construct,
+        CAP_NET_BLOCK_LIST => crate::modules::net::block_list_construct,
         CAP_HTTP_SERVER => http_create_server_construct,
         CAP_HTTP_AGENT => crate::modules::http_client::agent_construct,
         CAP_BUFFER_NEW => buffer_new_construct,
