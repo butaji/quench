925f94b9b2b06f69dcc8bcb97cca584e8167c8ea
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-27 11:34:33 -0500

    assert: validate thenable catch capability

--- tests/node-compat/stage-2899/test-assert-thenable-validation.js
+++ tests/node-compat/stage-2899/test-assert-thenable-validation.js
@@ -23,3 +23,21 @@ assert.rejects(rejectingFn, {
   name: 'AssertionError',
   message: 'Failed',
 });
+
+const err = new Error('foobar');
+const validate = () => 'baz';
+assert.rejects(
+  assert.rejects(Promise.reject(err), validate),
+  { code: 'ERR_ASSERTION', operator: 'rejects', actual: err, expected: validate }
+);
+
+assert.rejects(
+  assert.doesNotReject(() => new Map()),
+  { code: 'ERR_INVALID_RETURN_VALUE', name: 'TypeError' }
+);
+
+assert.rejects(assert.doesNotReject(Promise.reject(new Error('Failed'))), {
+  code: 'ERR_ASSERTION', operator: 'doesNotReject'
+});
+assert.doesNotReject(async () => {});
+assert.doesNotReject(Promise.resolve());
