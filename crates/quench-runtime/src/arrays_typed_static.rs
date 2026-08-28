2b300bf7caa5888223e5b2f4d6391783603d41e4
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-28 01:03:31 -0500

    validate typed array from constructor receiver

--- crates/quench-runtime/src/arrays_typed_static.rs
+++ crates/quench-runtime/src/arrays_typed_static.rs
@@ -4,10 +4,22 @@ fn typed_array_static(
     arguments: &[Value],
 ) -> Option<Result<Value, crate::execute::VmError>> {
     match builtin {
-        crate::ops::Builtin::TypedArrayFrom => Some(from(receiver, arguments)),
+        crate::ops::Builtin::TypedArrayFrom => Some(typed_array_from(receiver, arguments)),
         crate::ops::Builtin::TypedArrayOf => Some(typed_array_of(receiver, arguments)),
         _ => None,
     }
+}
+
+fn typed_array_from(
+    receiver: Option<&Value>,
+    arguments: &[Value],
+) -> Result<Value, crate::execute::VmError> {
+    let Some(receiver) = receiver.filter(|value| is_constructor(value)) else {
+        return Err(crate::value::error::throw_type_error(
+            "TypedArray.from called on a non-constructor",
+        ));
+    };
+    from(Some(receiver), arguments)
 }
 
 fn typed_array_of(
