dae5bbb00c4bdff7ae4584535c41578984e23d40
Vitaly Baum <vitaly.baum@gmail.com>
2026-08-30 07:27:58 -0500

    reset fixture execution state completely

--- crates/quench-runtime/src/lib.rs
+++ crates/quench-runtime/src/lib.rs
@@ -128,7 +128,10 @@ mod with_scope;
 pub fn reset_fixture_caches() {
     construct::reset_weak_refs();
     global_environment::reset_global_bindings();
+    loops::reset_fixture_state();
     module_bindings::reset_module_jobs();
+    private_environment::reset_fixture_state();
+    reflect::reset_fixture_state();
     promise::reset_fixture_state();
     templates::reset_tagged_template_cache();
     value::reset_object_layout_cache();
