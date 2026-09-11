eaf8ce6e79eb9179967b8280f7d4ac232fbdd4fc
Vitaly Baum <vitaly.baum@gmail.com>
2026-09-11 12:33:19 -0500

    fs: expose FileHandle appendFile and writeFile

--- crates/quench-node/src/dispatch_fs.rs
+++ crates/quench-node/src/dispatch_fs.rs
@@ -88,6 +88,8 @@ pub fn fs_dispatch(cap: u16) -> Option<CallHandler> {
         CAP_FS_HANDLE_STAT => crate::modules::fs::file_handle_stat,
         CAP_FS_HANDLE_CHMOD => crate::modules::fs::file_handle_chmod,
         CAP_FS_HANDLE_TRUNCATE => crate::modules::fs::file_handle_truncate,
+        CAP_FS_HANDLE_APPENDFILE => crate::modules::fs::file_handle_append_file,
+        CAP_FS_HANDLE_WRITEFILE => crate::modules::fs::file_handle_write_file,
         CAP_FS_READSTREAM_CLOSE => crate::modules::fs::read_stream_close,
         CAP_FS_READSTREAM_DESTROY => crate::modules::fs::read_stream_destroy,
         CAP_FS_UTF8STREAM_WRITE => crate::modules::fs_utf8_stream::write,
